8939770803225d12d6bc7ce15e654026ca70ed74
Unknown <maxg@kmail.org>
2017-11-15 23:13:41 -0800

    More testing with JSON and Python

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import base64
 import hmac
 import hashlib
 from hashlib import sha384
+import json
 
 url = "https://api.gemini.com/v1/order/status"
 
@@ -17,6 +18,19 @@ b64 = base64.b64encode("""{
     "order_id": 18834
 }
 """)
+
+json_string = """{
+    "request": "/v1/order/status",
+    "nonce": 123456,
+    "order_id": 18834
+}
+"""
+print json_string
+json_string2 = json.dumps({'request': '/v1/order/status', 'nonce': 123456, 'order_id': 18834}, indent=4, separators=(',', ': '))
+print json_string2
+print base64.b64encode(json_string2)
+print base64.b64encode(json_string)
+print b64
 
 signature = hmac.new("1234abcd", b64, hashlib.sha384).hexdigest()
 
